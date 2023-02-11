232d63ab3e485f6accb7dbfbc9a6119a50f12333
dipali sorthiya <dsorathiya77@gmail.com>
2023-02-11 18:25:04 +0530

    till video 52

---
+++ src/components/form/Title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Title({ children }) {
+  return (
+    <h1 className="text-xl text-white font-semibold text-center">{children}</h1>
+  );
+}
+export default Title;
